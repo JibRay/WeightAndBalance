1d3fb271c30d0b19f719e8c5315a49c6b938ecb3
Jib Ray <jib2ray@gmail.com>
2023-06-12 05:58:11 -0700

    Initial Commit

---
+++ WeightAndBalance/WeightAndBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightAndBalanceApp.swift
+//  WeightAndBalance
+//
+//  Created by Jib Ray on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeightAndBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
